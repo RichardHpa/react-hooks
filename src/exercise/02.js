81a3d7e7b06c93e22260f983d5798c2148d46031
RichardHpa <richard.hpa@hotmail.com>
2021-05-24 22:59:03 +1200

    completed tictactoe

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,18 +3,40 @@
 
 import * as React from 'react'
 
+// Custom Hook for saving value into local storage
+const useLocalStorageState = (key, defaultValue = '') => {
+  const getInitialValue = () => window.localStorage.getItem(key) || defaultValue
+
+  const [state, setState] = React.useState(getInitialValue)
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, state)
+  }, [key, state])
+
+  return [state, setState]
+}
+
 function Greeting({initialName = ''}) {
+  const [name, setName] = useLocalStorageState('name', initialName)
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
+  // const getInitialValue = () =>
+  //   window.localStorage.getItem('name') || initialName
+
+  // const [name, setName] = React.useState(getInitialValue)
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
 
+  // React.useEffect(() => {
+  //   window.localStorage.setItem('name', name)
+  // }, [name])
+
   function handleChange(event) {
     setName(event.target.value)
   }
+
   return (
     <div>
       <form>
